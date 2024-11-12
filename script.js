2d10db0ca23346973adf9815021981a195f8c08b
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-12 16:28:36 +0100

    introJS

---
+++ script.js
@@ -0,0 +1,159 @@
+//console.log("hello world");
+
+//let variable1;
+//let variable2;
+
+//variable1 = 9;
+//variable2 = 5;
+
+//let stockage;
+
+//stockage = variable1;
+//variable1 = variable2;
+//variable2 = stockage;
+
+//console.log(stockage);
+
+/*let h = "h";
+let e = "e";
+let l = "l";
+let o = "o";
+let w = "w";
+let r = "r";
+let d = "d";
+
+let helloworld =
+  h +
+  "\n" +
+  e +
+  "\n" +
+  l +
+  "\n" +
+  l +
+  "\n" +
+  o +
+  "\n" +
+  " " +
+  "\n" +
+  w +
+  "\n" +
+  o +
+  "\n" +
+  r +
+  "\n" +
+  l +
+  "\n" +
+  d +
+  "\n" +
+  " "+
+  "\n" +
+  "!";
+
+console.log(helloworld);*/
+
+/*let age = 50;
+
+if (age >= 18) {
+    console.log("L'utilisateur est majeur.")
+} else {
+    console.log("L'utilisateur est mineur.")
+}*/
+
+/*for (let i=15; i>=0; i = i-2){
+  console.log(i)
+}*/
+
+/*
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let letter = "D";
+
+for (let i = 0; i <= alphabet.length; i = i + 1) {
+  if (alphabet[i] === letter) {
+    console.log(alphabet[i])
+    console.log(i + 1);
+    break;
+  }
+}
+*/
+
+//console.log(alphabet.indexOf("S"));
+
+/*
+function findTheLetter(letter){
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  for (let i = 0; i <= alphabet.length; i = i + 1) {
+    if (alphabet[i] === letter) {
+      return letter + "\n" + (i+1); 
+    
+    }
+  }
+}
+console.log(findTheLetter("E"));
+*/
+
+/*
+function surfaceRectangle(L, l) {
+  if (L === l) {
+    return L * l + "cm², et je suis suis un carré";
+  } else {
+    return L * l + "cm², et je suis un rectangle";
+  }
+}
+console.log(surfaceRectangle(5, 1));
+*/
